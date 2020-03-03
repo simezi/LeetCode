d03177fb91e1fca81da55373222151eb57180895
simezi a.k.a matzz <matsubaray@binc.jp>
2020-03-03 22:44:17 +0900

    Update 0029. Divide Two Integers.js

--- 0029. Divide Two Integers.js
+++ 0029. Divide Two Integers.js
@@ -1,4 +1,23 @@
 /**
+ * fast with log
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+var divide = function(dividend, divisor) {
+  if (dividend == Math.pow(-2, 31) && divisor == -1) {
+    return Math.pow(2, 31) - 1;
+  }
+  const dividendSign = dividend < 0 ? -1 : 1;
+  const divisorSign = divisor < 0 ? -1 : 1;
+  const udividend = dividendSign == -1 ? 0 - dividend : dividend
+  const udivisor = divisorSign == -1 ? 0 - divisor : divisor
+  return divisorSign === dividendSign
+    ? parseInt(Math.exp(Math.log(udividend) - Math.log(udivisor)))
+    : 0 - parseInt(Math.exp(Math.log(udividend) - Math.log(udivisor)));
+};
+
+/**
  * slow primitive version
  * @param {number} dividend
  * @param {number} divisor
